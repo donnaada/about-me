e64a22007cd07a1984aae1a75a36a76ddf1e8ecb
Donna M. Ada <donnaada@icloud.com>
2023-03-09 03:07:10 +1000

    fixing eslint issues

--- js/app.js
+++ js/app.js
@@ -1,73 +1,73 @@
 'use strict';
 
 let visitorName = prompt('Hello, what is your name?');
-if (visitorName == '' || visitorName === null){
-    visitorName = "Visitor";
+if (visitorName === '' || visitorName === null){
+  visitorName = 'Visitor';
 }
 
-let choice = confirm(`Hello ${visitorName}! Welcome to my website about me! Its so nice to have you here. Before we begin, would you like to play a guessing game?`)
+let choice = confirm(`Hello ${visitorName}! Welcome to my website about me! Its so nice to have you here. Before we begin, would you like to play a guessing game?`);
 
 
 if (!choice){
 
-    console.log(choice);
+  console.log(choice);
 
 } else {
 
-let guessOrigin = prompt('Question 1/5: I am from Saipan, have you ever heard of it or do you know where that is? Enter Y or N');
+  let guessOrigin = prompt('Question 1/5: I am from Saipan, have you ever heard of it or do you know where that is? Enter Y or N');
 
-if (guessOrigin.toLowerCase() === "y" || guessOrigin.toLowerCase() === "yes"){
-    alert(`Answer: ${guessOrigin}. Thats awesome! I don\'t always meet people that know where it is.`)
-} else if (guessOrigin.toLowerCase() === "n" || guessOrigin.toLowerCase() === "no"){
+  if (guessOrigin.toLowerCase() === 'y' || guessOrigin.toLowerCase() === 'yes'){
+    alert(`Answer: ${guessOrigin}. Thats awesome! I don't always meet people that know where it is.`);
+  } else if (guessOrigin.toLowerCase() === 'n' || guessOrigin.toLowerCase() === 'no'){
     alert(`Answer: ${guessOrigin}. It's okay. There is a link on my page to Google Maps to show you where it is :) `);
-} else {
-    alert(`${guessOrigin} was not one of the choices. On to the next question.`)
-}
+  } else {
+    alert(`${guessOrigin} was not one of the choices. On to the next question.`);
+  }
 
-let isWorking = prompt('Question 2/5: Am I Employed Full Time and while attending this coding bootcamp? Y or N?');
+  let isWorking = prompt('Question 2/5: Am I Employed Full Time and while attending this coding bootcamp? Y or N?');
 
-if (isWorking.toLowerCase() === "y" || isWorking.toLowerCase() === "yes"){
-    alert(`Answer: ${isWorking}. Yes, I am working full time!`)
-} else if (isWorking.toLowerCase() === "n" || isWorking.toLowerCase() === "no"){
+  if (isWorking.toLowerCase() === 'y' || isWorking.toLowerCase() === 'yes'){
+    alert(`Answer: ${isWorking}. Yes, I am working full time!`);
+  } else if (isWorking.toLowerCase() === 'n' || isWorking.toLowerCase() === 'no'){
     alert(`Answer: ${isWorking}. No, I am employed Full Time!`);
-} else {
-    alert(`${isWorking} was not one of the choices. On to the next question.`)
-}
-let enjoysBeach = prompt('Question 3/5: I live on an island where its summer year round, is my favorite outdoor activity swimming or going to the beach? Y or N?');
+  } else {
+    alert(`${isWorking} was not one of the choices. On to the next question.`);
+  }
+  let enjoysBeach = prompt('Question 3/5: I live on an island where its summer year round, is my favorite outdoor activity swimming or going to the beach? Y or N?');
 
-if (enjoysBeach.toLowerCase() === "y" || enjoysBeach.toLowerCase() === "yes"){
-    alert(`Answer: ${enjoysBeach}. Actually, I prefer snowboarding`)
-} else if (enjoysBeach.toLowerCase() === "n" || enjoysBeach.toLowerCase() === "no"){
+  if (enjoysBeach.toLowerCase() === 'y' || enjoysBeach.toLowerCase() === 'yes'){
+    alert(`Answer: ${enjoysBeach}. Actually, I prefer snowboarding`);
+  } else if (enjoysBeach.toLowerCase() === 'n' || enjoysBeach.toLowerCase() === 'no'){
     alert(`Answer: ${enjoysBeach}. You guessed right! I don't really enjoy swimming or going to the beach.`);
-} else {
-    alert(`${enjoysBeach} was not one of the choices. On to the next question.`)
-}
+  } else {
+    alert(`${enjoysBeach} was not one of the choices. On to the next question.`);
+  }
 
-let enjoysHiking = prompt('Question 4/5: Do I enjoy Hiking? Y or N?');
+  let enjoysHiking = prompt('Question 4/5: Do I enjoy Hiking? Y or N?');
 
-if (enjoysHiking.toLowerCase() === "y" || enjoysHiking.toLowerCase() === "yes"){
-    alert(`Answer: ${enjoysHiking}. I do, as a matter of fact, enjoy hiking :) `)
-} else if (enjoysHiking.toLowerCase() === "n" || enjoysHiking.toLowerCase() === "no"){
+  if (enjoysHiking.toLowerCase() === 'y' || enjoysHiking.toLowerCase() === 'yes'){
+    alert(`Answer: ${enjoysHiking}. I do, as a matter of fact, enjoy hiking :) `);
+  } else if (enjoysHiking.toLowerCase() === 'n' || enjoysHiking.toLowerCase() === 'no'){
     alert(`Answer: ${enjoysHiking}. Actually, I don't mind hiking at all.`);
-} else {
-    alert(`${enjoysHiking} was not one of the choices. On to the last question.`)
-}
+  } else {
+    alert(`${enjoysHiking} was not one of the choices. On to the last question.`);
+  }
 
-let remote = prompt('Question 5/5: Is one of my goals to work 100% Remote? Y or N?');
+  let remote = prompt('Question 5/5: Is one of my goals to work 100% Remote? Y or N?');
 
-if (remote.toLowerCase() === "y" || remote.toLowerCase() === "yes"){
-    alert(`Answer: ${remote}. That is 1000% correct!`)
-} else if (remote.toLowerCase() === "n" || remote.toLowerCase() === "no"){
+  if (remote.toLowerCase() === 'y' || remote.toLowerCase() === 'yes'){
+    alert(`Answer: ${remote}. That is 1000% correct!`);
+  } else if (remote.toLowerCase() === 'n' || remote.toLowerCase() === 'no'){
     alert(`Answer: ${remote}. The correct answer is yes. I am hoping that one day I would be able to find a job that allows me to be 100% remote.`);
-} else {
-    alert(`${remote} was not one of the choices. On to the website.`)
-}
+  } else {
+    alert(`${remote} was not one of the choices. On to the website.`);
+  }
 
-alert(`Thank you for taking the time to answer the questions! `)
+  alert('Thank you for taking the time to answer the questions! ');
 }
 
 alert(`I hope you enjoy the website ${visitorName} :) `);
 
 
 let placeholder = document.getElementById('customWelcome');
-placeholder.innerHTML = `Hafa Adai and Tirow ${visitorName}! <br> Welcome to my site! `
+placeholder.innerHTML = `Hafa Adai and Tirow ${visitorName}! <br> Welcome to my site! `;
