12ce6f39c2b53682c7bd2030773e53474bf2d319
Donna M. Ada <donnaada@icloud.com>
2023-03-09 20:18:28 +1000

    fixing eslint issues

--- js/app.js
+++ js/app.js
@@ -118,65 +118,53 @@ alert(score);
 alert('Just Kidding, we have a few more questions :) ');
 
 
-  // gets guessInput using prompt; converts to integer and stores value in rating and declares star variable with blank string.
-  let attempt = 1;
-  let maxAttempts = 4;
-  let attemptLeft = 4;
+// gets guessInput using prompt; converts to integer and stores value in rating and declares star variable with blank string.
+let attempt = 1;
+let maxAttempts = 4;
+let attemptLeft = 4;
 
-  let guessInput = prompt(`Guess how many sister-in-laws I have. (Pick a number from 1-10). Attempts Left: ${attemptLeft}`);
-  let guess = parseInt(guessInput);
-  let sil = 6;
+let guessInput = prompt(`Guess how many sister-in-laws I have. (Pick a number from 1-10). Attempts Left: ${attemptLeft}`);
+let guess = parseInt(guessInput);
+let sil = 6;
 
+while (guessInput === null || isNaN(guess)){
+  // guess = 0;
+  alert('that was an an invalid response.');
+  guessInput = prompt(`Guess how many sister-in-laws I have. (Pick a number from 1-10). ${attemptLeft} tries remaining.`);
+  guess = parseInt(guessInput);
+}
 
-// while (guess != sil && attempt < maxAttempts){
+while (guess !== sil && attempt < maxAttempts){
 
-    while (guessInput === null || isNaN(guess)){
-        // guess = 0;
-        alert('that was an an invalid response.')
-        guessInput = prompt(`Guess how many sister-in-laws I have. (Pick a number from 1-10). ${attemptLeft} tries remaining.`);
-        guess = parseInt(guessInput);
-    } 
+  //While loop to continiously prompt user to enter number until it is between 1- 10
+  while (guess < 1 || guess > 10){
+    alert(`${guess} is not between 1 and 10. Please try again!`);
+    guess = parseInt(prompt('You have '+ attemptLeft + ' tries to guess how many sister-in-laws I have. (Pick a number from 1-10).'));
+  }
 
-    while (guess != sil && attempt < maxAttempts){
+  attemptLeft--;
 
-            //While loop to continiously prompt user to enter number until it is between 1- 10
-    while (guess < 1 || guess > 10){
-        alert(`${guess} is not between 1 and 10. Please try again!`);
-        guess = parseInt(prompt("You have "+ attemptLeft + " tries to guess how many sister-in-laws I have. (Pick a number from 1-10)."));
-    }
+  if (guess < sil){
+    alert(`${guess} is wrong. Try a higher number.`);
+  }
 
+  if (guess > sil) {
+    alert(`${guess} is wrong. Try a lower number.`);
+  }
 
-        attemptLeft--;
+  guess = parseInt(prompt('You have '+ attemptLeft + ' tries to guess how many sister-in-laws I have. (Pick a number from 1-10).'));
+  attempt ++;
+}
 
-         if (guess < sil){
-            alert(`${guess} is wrong. Try a higher number.`)
-        } 
+if (guess === sil){
+  alert(`Thank you for playing! You didn't guess it in the it in the ${attempt} tries. I have ${sil} sister-in-laws`);
+}
 
-        if (guess > sil) {
-            alert(`${guess} is wrong. Try a lower number.`)
-        }
-
-        guess = parseInt(prompt("You have "+ attemptLeft + " tries to guess how many sister-in-laws I have. (Pick a number from 1-10)."));
-
-        attempt ++;
-
-    }
-
-    if (guess === sil){
-        alert(`Thank you for playing! You didn't guess it in the it in the ${attempt} tries. I have ${sil} sister-in-laws`);
-    }
-
-    if (guess !== sil){
-        alert(`Thank you for playing! You didn't guess it in the it in the ${attempt} tries. I have ${sil} sister-in-laws`);
-    }
-
-
-
-
-
+if (guess !== sil){
+  alert(`Thank you for playing! You didn't guess it in the it in the ${attempt} tries. I have ${sil} sister-in-laws`);
+}
 
 alert(`I hope you enjoy the website ${visitorName} :) `);
-
 
 let placeholder = document.getElementById('customWelcome');
 placeholder.innerHTML = `Hafa Adai and Tirow ${visitorName}! <br> Welcome to my site! `;
