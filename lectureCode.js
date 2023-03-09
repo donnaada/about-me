d3e727d1a71dc379f6e69779b53e40bee86df0ce
Donna M. Ada <donnaada@icloud.com>
2023-03-10 05:55:04 +1000

    adding function from lecture

---
+++ lectureCode.js
@@ -0,0 +1,25 @@
+function askQuestion(question, answer) {
+  let response = prompt(question).toLowerCase;
+  if (parseInt(response)){
+    response = parseInt(response);
+  }
+
+  if (response === answer) {
+    console.log('Correct!');
+  } else {
+    console.log('Wrong!');
+  }
+}
+
+let question = ['question1', 'question2', 'question3'];
+let answer = ['answer1', 'answer2', 'answer3'];
+
+for (let i = 0; i < question.length; i++) {
+  let isCorrect = askQuestion(question[i], answer[i]);
+
+  if (isCorrect) {
+    console.log('Correct!');
+  } else {
+    console.log('Wrong!');
+  }
+}
